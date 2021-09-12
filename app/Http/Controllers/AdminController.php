d3b6c098c22c3f025266a9982194b6d4d8d81b33
DESKTOP-MUJTGOT\Ibn-e-Yunus <salmanabbas985@gmail.com>
2021-09-12 16:25:27 +0300

    removed nonrelated funtions

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,44 +12,4 @@ class AdminController extends Controller
         return view('admin.dashboard');
     }
 
-    public function products()
-    {
-
-        $products = Product::paginate(8);
-
-        return view('admin.product.index')->with(['products' => $products]);
-    }
-
-    public function create()
-    {
-        return view('admin.product.create');
-    }
-
-    public function store(Request $request)
-    {
-        $request->validate([
-            'category_id' => 'required|int',
-            'name' => 'required|string',
-            'img' => 'required|mimes:png',
-            'color' => 'required|string',
-            'size' => 'required|string',
-            'rating' => 'required|int',
-            'description' => 'required|min:10|max:1000',
-            'amount' => 'required',
-        ]);
-        dd($request->all());
-        // Product::create([
-        //     'category_id' => $request->category_id,
-        //     'name' => $request->name,
-        //     'img' => $request->img,
-        //     'color' => $request->color,
-        //     'size' => $request->size,
-        //     'rating' => $request->rating,
-        //     'description' => $request->description,
-        //     'amount' => $request->amount,
-        // ]);
-
-    }
-
-
 }
