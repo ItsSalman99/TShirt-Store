528d0555c1db27a4ed5effbe8c828878b610bac2
DESKTOP-2\Hp <salmanabbas985@gmail.com>
2022-02-09 21:27:22 +0500

    for heroku

--- config/database.php
+++ config/database.php
@@ -66,8 +66,8 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => $DATABASE_URL["host"],
-            'port' => $DATABASE_URL["port"],
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
